03e24cd79e897644bbbce408d3329aeeea08399d
Abdoelsamea <Abdoel.html@gmail.com>
2018-06-18 22:19:52 -0400

    Renamed AuthScreen to MainScreen

--- Airbnb/App.js
+++ Airbnb/App.js
@@ -1,12 +1,12 @@
 import { Navigation } from 'react-native-navigation';
-import AuthScreen from './src/Screens/AuthScreen/AuthScreen';
+import MainScreen from './src/Screens/MainScreen/MainScreen';
 import MoreOptions from './src/Screens/MoreOptions/MoreOptions';
 
-Navigation.registerComponent('Airbnb.AuthScreen', () => AuthScreen);
+Navigation.registerComponent('Airbnb.MainScreen', () => MainScreen);
 Navigation.registerComponent('Airbnb.MoreOptions', () => MoreOptions);
 
 Navigation.startSingleScreenApp({
 	screen: {
-		screen: 'Airbnb.AuthScreen',
+		screen: 'Airbnb.MainScreen',
 	},
 });
